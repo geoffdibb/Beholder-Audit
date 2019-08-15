653a98fefe04ea546e2b6748c4580f1276d308cf
patelkarishma10 <patelkarishma10@gmail.com>
2019-08-15 13:48:27 +0100

    created springboot api

---
+++ src/main/java/com/bae/BeholderAuditApplication.java
@@ -0,0 +1,13 @@
+package com.bae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeholderAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeholderAuditApplication.class, args);
+	}
+
+}
